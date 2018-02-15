81f96904e913b07a5fa3fd3d0cea1fc3025bd3ac
RoanBuysse <roan.buysse@telenet.be>
2018-02-15 13:53:09 +0100

    Basic Layout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,6 +26,79 @@
                         </div>
 
                         <div class="form-group row">
+                                <label for="surname" class="col-md-4 col-form-label text-md-right">Surname</label>
+    
+                                <div class="col-md-6">
+                                    <input id="surname" type="text" class="form-control{{ $errors->has('surname') ? ' is-invalid' : '' }}" name="surname" value="{{ old('surname') }}" required autofocus>
+    
+                                    @if ($errors->has('surname'))
+                                        <span class="invalid-feedback">
+                                            <strong>{{ $errors->first('surname') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                    <label for="postal_code" class="col-md-4 col-form-label text-md-right">Postal code</label>
+        
+                                    <div class="col-md-6">
+                                        <input id="postal_code" type="text" class="form-control{{ $errors->has('postal_code') ? ' is-invalid' : '' }}" name="postal_code" value="{{ old('surname') }}" required autofocus>
+        
+                                        @if ($errors->has('postal_code'))
+                                            <span class="invalid-feedback">
+                                                <strong>{{ $errors->first('postal_code') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+
+                            <div class="form-group row">
+                                    <label for="country" class="col-md-4 col-form-label text-md-right">Country</label>
+        
+                                    <div class="col-md-6">
+                                        <input id="country" type="text" class="form-control{{ $errors->has('country') ? ' is-invalid' : '' }}" name="country" value="{{ old('country') }}" required autofocus>
+        
+                                        @if ($errors->has('country'))
+                                            <span class="invalid-feedback">
+                                                <strong>{{ $errors->first('country') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                        <label for="language" class="col-md-4 col-form-label text-md-right">Language</label>
+            
+                                        <div class="col-md-6">
+                                            <input id="language" type="text" class="form-control{{ $errors->has('postal_code') ? ' is-invalid' : '' }}" name="language" value="{{ old('language') }}" required autofocus>
+            
+                                            @if ($errors->has('language'))
+                                                <span class="invalid-feedback">
+                                                    <strong>{{ $errors->first('language') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
+                                    </div>
+
+
+                            <div class="form-group row">
+                                    <label for="postal_code" class="col-md-4 col-form-label text-md-right">Postal code</label>
+        
+                                    <div class="col-md-6">
+                                        <input id="postal_code" type="text" class="form-control{{ $errors->has('postal_code') ? ' is-invalid' : '' }}" name="postal_code" value="{{ old('postal_code') }}" required autofocus>
+        
+                                        @if ($errors->has('postal_code'))
+                                            <span class="invalid-feedback">
+                                                <strong>{{ $errors->first('postal_code') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+    
+
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
 
                             <div class="col-md-6">
@@ -60,6 +133,11 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
+
+
+                      
+
+
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Pride.be</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -18,7 +18,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'Pride.be') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -27,6 +27,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                            <li><a class="nav-link" href="">Agenda</a></li>
+                            <li><a class="nav-link" href="">Nieuws</a></li>
+
 
                     </ul>
 
