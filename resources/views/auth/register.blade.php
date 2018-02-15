a519602538bb2a4c68d0bf5b1818863bd7f090f7
RoanBuysse <roan.buysse@telenet.be>
2018-02-15 14:42:17 +0100

    author package added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,14 +40,14 @@
                             </div>
 
                             <div class="form-group row">
-                                    <label for="postal_code" class="col-md-4 col-form-label text-md-right">Postal code</label>
+                                    <label for="city" class="col-md-4 col-form-label text-md-right">City</label>
         
                                     <div class="col-md-6">
-                                        <input id="postal_code" type="text" class="form-control{{ $errors->has('postal_code') ? ' is-invalid' : '' }}" name="postal_code" value="{{ old('surname') }}" required autofocus>
+                                        <input id="city" type="text" class="form-control{{ $errors->has('postal_code') ? ' is-invalid' : '' }}" name="city" value="{{ old('surname') }}" required autofocus>
         
-                                        @if ($errors->has('postal_code'))
+                                        @if ($errors->has('city'))
                                             <span class="invalid-feedback">
-                                                <strong>{{ $errors->first('postal_code') }}</strong>
+                                                <strong>{{ $errors->first('city') }}</strong>
                                             </span>
                                         @endif
                                     </div>
@@ -68,34 +68,9 @@
                                     </div>
                                 </div>
 
-                                <div class="form-group row">
-                                        <label for="language" class="col-md-4 col-form-label text-md-right">Language</label>
-            
-                                        <div class="col-md-6">
-                                            <input id="language" type="text" class="form-control{{ $errors->has('postal_code') ? ' is-invalid' : '' }}" name="language" value="{{ old('language') }}" required autofocus>
-            
-                                            @if ($errors->has('language'))
-                                                <span class="invalid-feedback">
-                                                    <strong>{{ $errors->first('language') }}</strong>
-                                                </span>
-                                            @endif
-                                        </div>
-                                    </div>
+                              
 
-
-                            <div class="form-group row">
-                                    <label for="postal_code" class="col-md-4 col-form-label text-md-right">Postal code</label>
-        
-                                    <div class="col-md-6">
-                                        <input id="postal_code" type="text" class="form-control{{ $errors->has('postal_code') ? ' is-invalid' : '' }}" name="postal_code" value="{{ old('postal_code') }}" required autofocus>
-        
-                                        @if ($errors->has('postal_code'))
-                                            <span class="invalid-feedback">
-                                                <strong>{{ $errors->first('postal_code') }}</strong>
-                                            </span>
-                                        @endif
-                                    </div>
-                                </div>
+                           
     
 
                         <div class="form-group row">
